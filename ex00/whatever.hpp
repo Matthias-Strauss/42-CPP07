862e47827a5698d5205e9e7c0f12019d7df4e1d1
Matthias Strauss <mstrauss@student.42heilbronn.de>
2025-04-01 13:45:43 +0200

    added function implementations. done in the .hpp file in order to use the advantages of template.

--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: mstrauss <mstrauss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 13:35:34 by mstrauss          #+#    #+#             */
-/*   Updated: 2025/04/01 13:38:46 by mstrauss         ###   ########.fr       */
+/*   Updated: 2025/04/01 13:44:55 by mstrauss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,18 @@
 #ifndef WHATEVER_HPP
 #define WHATEVER_HPP
 
-template <typename T> void swap(T &a, T &b) {}
+template <typename T> void swap(T &a, T &b) {
+  T tmp = a;
+  a = b;
+  b = tmp;
+}
 
-template <typename T> const T &min(const T &a, const T &b) {}
+template <typename T> const T &min(const T &a, const T &b) {
+  return (a < b ? a : b);
+}
 
-template <typename T> const T &max(const T &a, const T &b) {}
+template <typename T> const T &max(const T &a, const T &b) {
+  return (a > b ? a : b);
+}
 
 #endif
