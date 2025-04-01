79828bb3a190d5212d24be72b713675aa65a6d45
Matthias Strauss <mstrauss@student.42heilbronn.de>
2025-04-01 14:29:27 +0200

    ex00: added additional tests and output formatting

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,27 +6,89 @@
 /*   By: mstrauss <mstrauss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 13:09:01 by mstrauss          #+#    #+#             */
-/*   Updated: 2025/04/01 13:46:07 by mstrauss         ###   ########.fr       */
+/*   Updated: 2025/04/01 14:26:39 by mstrauss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
+#include <array>
 #include <iostream>
 
 int main(void) {
   int a = 2;
   int b = 3;
 
+  std::cout << std::endl;
+  std::cout << "initializing int a = " << a << ";" << std::endl;
+  std::cout << "initializing int b = " << b << ";" << std::endl;
+  std::cout << std::endl;
+
+  std::cout << "... swapping a with b ..." << std::endl;
   ::swap(a, b);
+
   std::cout << "a = " << a << ", b = " << b << std::endl;
+  std::cout << std::endl;
+
+  std::cout << "... determining minimum of a & b ..." << std::endl;
   std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+  std::cout << std::endl;
+
+  std::cout << "... determining maximum of a & b ..." << std::endl;
   std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+  std::cout << std::endl;
+
   std::string c = "chaine1";
   std::string d = "chaine2";
+  std::cout << "initializing std::string c = " << "\"" << c << "\""
+            << std::endl;
+  std::cout << "initializing std::string d = " << "\"" << d << "\""
+            << std::endl;
+  std::cout << std::endl;
+
   ::swap(c, d);
   std::cout << "c = " << c << ", d = " << d << std::endl;
   std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
   std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+  std::cout << std::endl;
+
+  std::cout << "#####################" << std::endl;
+  std::cout << "## ADDITIONAL TESTS #" << std::endl;
+  std::cout << "#####################" << std::endl;
+  std::cout << "" << std::endl;
+
+  std::array<int, 5> arr1 = {1, 2, 3, 4, 5};
+  std::cout << "initializing arr1: ";
+  for (int i = 0; i < 5; ++i) {
+    std::cout << arr1[i] << " ";
+  }
+  std::cout << std::endl;
+
+  std::array<int, 5> arr2 = {5, 4, 3, 2, 1};
+  std::cout << "initializing arr2: ";
+  for (int i = 0; i < 5; ++i) {
+    std::cout << arr2[i] << " ";
+  }
+  std::cout << std::endl;
+
+  std::cout << std::endl;
+  std::cout << "... swapping arr1 with arr2 ..." << std::endl;
+  std::cout << std::endl;
+
+  ::swap(arr1, arr2);
+
+  std::cout << "arr1: ";
+  for (int i = 0; i < 5; ++i) {
+    std::cout << arr1[i] << " ";
+  }
+  std::cout << std::endl;
+
+  std::cout << "arr2: ";
+  for (int i = 0; i < 5; ++i) {
+    std::cout << arr2[i] << " ";
+  }
+  std::cout << std::endl;
+  std::cout << std::endl;
+
   return 0;
 }
 
