5f1113523e0a1b2840831b64a317c3d3c4e885ef
Matthias Strauss <mstrauss@student.42heilbronn.de>
2025-04-01 14:34:53 +0200

    ex01: Makefile and iter scaffolding

---
+++ ex01/iter.hpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mstrauss <mstrauss@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/01 14:31:05 by mstrauss          #+#    #+#             */
+/*   Updated: 2025/04/01 14:34:22 by mstrauss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+#ifndef ITER_HPP
+#define ITER_HPP
+
+template <typename T> void iter(std::array<int> arr1, u_int len, *f()) {}
+#endif
