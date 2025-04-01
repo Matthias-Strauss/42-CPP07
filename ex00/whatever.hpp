83c0eb4777fa61c3df7d42211a55d683ee49cc27
Matthias Strauss <mstrauss@student.42heilbronn.de>
2025-04-01 13:40:31 +0200

    gitignore. scaffolding for ex00

---
+++ ex00/whatever.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mstrauss <mstrauss@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/01 13:35:34 by mstrauss          #+#    #+#             */
+/*   Updated: 2025/04/01 13:38:46 by mstrauss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+template <typename T> void swap(T &a, T &b) {}
+
+template <typename T> const T &min(const T &a, const T &b) {}
+
+template <typename T> const T &max(const T &a, const T &b) {}
+
+#endif
