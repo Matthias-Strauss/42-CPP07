80cb9ea230b8f3e3e465b04ea83eff605de57ad0
Matthias Strauss <mstrauss@student.42heilbronn.de>
2025-04-03 13:23:24 +0200

    ex02: added test from subject to main

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,13 +6,16 @@
 /*   By: mstrauss <mstrauss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 16:42:44 by mstrauss          #+#    #+#             */
-/*   Updated: 2025/04/01 16:46:47 by mstrauss         ###   ########.fr       */
+/*   Updated: 2025/04/03 13:22:32 by mstrauss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
+#include <iostream>
 
-template <typename T> class Array {
-private:
-public:
+int main(void) {
+  int *a = new int();
+
+  std::cout << a << std::endl;
+  return 0;
 }
