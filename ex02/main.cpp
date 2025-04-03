da18291efbe0b87f858796df5237071c71ad2002
Matthias Strauss <mstrauss@student.42heilbronn.de>
2025-04-03 15:56:46 +0200

    ex02: fixed leak in main tests. deleted duplicate main.cpp

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,18 +6,26 @@
 /*   By: mstrauss <mstrauss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 16:42:44 by mstrauss          #+#    #+#             */
-/*   Updated: 2025/04/03 15:40:04 by mstrauss         ###   ########.fr       */
+/*   Updated: 2025/04/03 15:52:27 by mstrauss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
 #include <iostream>
 
+#define MAX_VAL 750
+
 int main(void) {
   int *a = new int();
 
+  std::cout << "std::cout << a << std::endl : ";
   std::cout << a << std::endl;
+  std::cout << "std::cout << *a << std::endl : ";
+  std::cout << *a << std::endl;
+  std::cout << std::endl;
+  delete a;
 
+  std::cout << "Creating Array with size: " << MAX_VAL << std::endl;
   Array<int> numbers(MAX_VAL);
   int *mirror = new int[MAX_VAL];
   srand(time(NULL));
@@ -28,7 +36,9 @@ int main(void) {
   }
   // SCOPE
   {
+    std::cout << "Creating tmp Array by copy" << std::endl;
     Array<int> tmp = numbers;
+    std::cout << "Creating test Array by copy of tmp" << std::endl;
     Array<int> test(tmp);
   }
 
@@ -39,11 +49,13 @@ int main(void) {
     }
   }
   try {
+    std::cout << "Trying to access numbers[-2]" << std::endl;
     numbers[-2] = 0;
   } catch (const std::exception &e) {
     std::cerr << e.what() << '\n';
   }
   try {
+    std::cout << "Trying to access numbers[MAX_VAL]" << std::endl;
     numbers[MAX_VAL] = 0;
   } catch (const std::exception &e) {
     std::cerr << e.what() << '\n';
@@ -53,5 +65,22 @@ int main(void) {
     numbers[i] = rand();
   }
   delete[] mirror; //
+
+  std::cout << "\n--- Testing with float ---" << std::endl;
+  Array<float> floatArray(5);
+  for (int i = 0; i < 5; ++i) {
+    floatArray[i] = i + 0.5f;
+    std::cout << "floatArray[" << i << "] = " << floatArray[i] << std::endl;
+  }
+
+  std::cout << "\n--- Testing with string ---" << std::endl;
+  Array<std::string> stringArray(3);
+  stringArray[0] = "Hello";
+  stringArray[1] = "World";
+  stringArray[2] = "!";
+  for (int i = 0; i < 3; ++i) {
+    std::cout << "stringArray[" << i << "] = " << stringArray[i] << std::endl;
+  }
+
   return 0;
 }
